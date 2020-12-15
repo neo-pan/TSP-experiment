change gnn layer type; fix a bug about negative sign

models/decoder.py
@@ -119,7 +119,7 @@ class AttentionDecoder(nn.Module):
             log_prob.view(bsz, num_heads, tgt_len, src_len).sum(dim=1) / num_heads
         )
 
-        return log_prob.squeeze()
+        return log_prob.squeeze(1)
 
     def precompute_keys(self, key: torch.Tensor) -> None:
         assert key.size(-1) == self.embed_dim

models/encoder.py
@@ -5,7 +5,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch_geometric.data import Data
-from torch_geometric.nn import GATConv as _GATConv
+from torch_geometric.nn import GATConv as _GATConv, TransformerConv as _TransformerConv
 
 
 def cal_size_list(in_dim, out_dim, layer_num):
@@ -56,11 +56,12 @@ class GATEncoder(nn.Module):
         self.embed_dim = embed_dim
         self.num_layers = num_layers
         self.heads = heads
+        assert (self.embed_dim % self.heads) == 0
         gnn_layer_list = []
         for _ in range(self.num_layers):
-            gnn_layer = _GATConv(
+            gnn_layer = _TransformerConv(
                 in_channels=self.embed_dim,
-                out_channels=self.embed_dim,
+                out_channels=self.embed_dim // self.heads,
                 heads=self.heads,
             )
             gnn_layer_list.append(gnn_layer)

models/tsp_agents.py
@@ -70,10 +70,10 @@ class TSPAgent(nn.Module):
         # Pooling graph features
         self._graph_feat = self.pooling_func(x, data.batch)
         # Precompute attention decoder keys based on graph node features
-        key = to_dense_batch(x, data.batch)[0]
+        key = self._dense_x
         # Change its shape from [batch_size, max_num_nodes, embed_dim]
         # to [max_num_nodes, batch_size, embed_dim] for attention layer
-        key = key.permute(1, 0, 2)
+        key = key.permute(1, 0, 2)  
         self._precomputed_k = self.decoder.precompute_keys(key)
 
         d = data.clone()
@@ -121,7 +121,7 @@ class TSPAgent(nn.Module):
         return query
 
     def _select_node(self, log_p: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
-        assert log_p.shape == mask.shape
+        assert log_p.shape == mask.shape, f"{log_p.shape}, {mask.shape}"
         probs = log_p.exp()
         if self.decode_type == "greedy":
             _, selected = probs.max(1)
@@ -173,7 +173,7 @@ class TSPCritic(nn.Module):
         # Pooling graph features
         graph_feat = self.pooling_func(x, data.batch)
         baseline = self.out_proj(graph_feat)
-        assert baseline.shape == target.shape
+        assert baseline.shape == target.shape, f"{baseline.shape}, {target.shape}"
         
         return baseline.detach(), F.mse_loss(baseline, target.detach())
 

train.py
@@ -19,7 +19,8 @@ from rl_algorithms.reinforce import reinforce_train_batch
 
 
 def warmup_baseline(baseline, dataset, env, optimizer, args):
-    for batch in DataLoader(dataset, args.warmup_batch_size):
+    print(f"Warmup Critic Baseline with training dataset")
+    for batch in tqdm(DataLoader(dataset, args.warmup_batch_size)):
         batch = batch.to(args.device)
         node_pos = to_dense_batch(batch.pos, batch.batch)[0]
         done = False
@@ -41,6 +42,7 @@ def validate(model, dataset, env, args):
     # Validate
     print("Validating...")
     cost = rollout(model, dataset, env, args)
+    assert cost.size(0)==len(dataset)
     avg_cost = -cost.mean()
     print(
         "Validation overall avg_cost: {} +- {}".format(
@@ -71,7 +73,7 @@ def rollout(model, dataset, env, args):
 
     return torch.cat(
         [
-            eval_model_bat(bat)
+            eval_model_bat(bat.to(args.device))
             for bat in DataLoader(dataset, batch_size=args.eval_batch_size)
         ],
         0,
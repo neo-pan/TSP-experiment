fix: bug of loss calculation reimplement of paper *Attention, Learn to Solve Routing Problems!*

environments/tsp.py
@@ -32,12 +32,12 @@ class TSPEnv(_BaseEnv):
         self._step_count += 1
         self._act_list.append(action)
         self._avail_mask[self._batch_idx, action.squeeze()] = False
-        
+
         # If not first step, update tour_len
         if self._step_count > 1:
             last_act = self._act_list[-2]
             self.tour_len += self._distance_matrix[self._batch_idx, last_act.squeeze(), action.squeeze()]
-        
+
         # If last step, complete tour_len, mask `done` as true
         if self._step_count == (self.num_nodes):
             done = True
@@ -46,16 +46,11 @@ class TSPEnv(_BaseEnv):
             raise ValueError
         else:
             done = False
-        
 
         reward = -self.tour_len if done else torch.zeros(self.batch_size)
 
         return (
-            TSPState(
-                first_node=self._act_list[0],
-                pre_node=action,
-                avail_mask=self._avail_mask.clone(),
-            ),
+            TSPState(first_node=self._act_list[0], pre_node=action, avail_mask=self._avail_mask.clone(),),
             reward,
             done,
             {},
@@ -90,11 +85,7 @@ class TSPEnv(_BaseEnv):
         self._act_list = []
         self.tour_len = torch.zeros((self.batch_size), dtype=torch.float, device=self.device)
 
-        return TSPState(
-            first_node=None,
-            pre_node=None,
-            avail_mask=self._avail_mask.clone(),
-        )
+        return TSPState(first_node=None, pre_node=None, avail_mask=self._avail_mask.clone(),)
 
     def __repr__(self) -> str:
         return f"TSP Environment, with {self.batch_size} graphs of size {self.num_nodes}"

models/decoder.py
@@ -20,7 +20,6 @@ class AttentionDecoder(nn.Module):
         self.num_heads = num_heads
         self.bias = bias
         self.tanh_clipping = tanh_clipping
-        self._precompute = False
         self.glimpse_proj = nn.Linear(self.embed_dim, self.embed_dim, bias=self.bias)
 
     def forward(
@@ -82,12 +81,12 @@ class AttentionDecoder(nn.Module):
         glimpse_K = glimpse_K.view(src_len, bsz, num_heads, head_dim).permute(2, 1, 0, 3)
         glimpse_V = glimpse_V.view(src_len, bsz, num_heads, head_dim).permute(2, 1, 0, 3)
 
-        compatibility = torch.matmul(glimpse_Q, glimpse_K.transpose(-1, -2)) / math.sqrt(glimpse_Q.size(-1))
+        compatibility = torch.matmul(glimpse_Q, glimpse_K.transpose(-2, -1)) / math.sqrt(glimpse_Q.size(-1))
         assert list(compatibility.size()) == [num_heads, bsz, tgt_len, src_len]
 
         if attn_mask is not None:
             assert attn_mask.dtype == torch.bool
-            compatibility.masked_fill_(heads_mask, float("-inf"))
+            compatibility.masked_fill_(heads_mask, -math.inf)
 
         heads = torch.matmul(torch.softmax(compatibility, dim=-1), glimpse_V)
         assert list(heads.size()) == [num_heads, bsz, tgt_len, head_dim]
@@ -103,8 +102,9 @@ class AttentionDecoder(nn.Module):
             logits = torch.tanh(logits) * self.tanh_clipping
         if attn_mask is not None:
             assert attn_mask.dtype == torch.bool
-            logits.masked_fill_(attn_mask, float("-inf"))
+            logits.masked_fill_(attn_mask, -math.inf)
 
         log_prob = F.log_softmax(logits, dim=-1)
+        assert not torch.isnan(log_prob).any()
 
         return log_prob.squeeze(1)

models/encoder.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Callable, Tuple
 
 import numpy as np
 import torch
@@ -13,7 +13,7 @@ from torch_geometric.nn import global_add_pool, global_max_pool, global_mean_poo
 from torch_geometric.utils import to_dense_batch
 
 
-def get_pooling_func(pooling_method: str):
+def get_pooling_func(pooling_method: str) -> Callable[[torch.Tensor, torch.Tensor], torch.Tensor]:
     pooling_funcs = {
         "add": global_add_pool,
         "max": global_max_pool,
@@ -23,13 +23,13 @@ def get_pooling_func(pooling_method: str):
     return pooling_funcs[pooling_method]
 
 
-def get_normalization_class(normalization: str):
+def get_normalization_class(normalization: str) -> nn.Module:
     norm_class = {"batch": BatchNorm, "instance": InstanceNorm}
     assert normalization in norm_class.keys(), f"Wrong normalization methon: {normalization}"
     return norm_class[normalization]
 
 
-def get_gnn_layer_class(gnn_layer: str):
+def get_gnn_layer_class(gnn_layer: str) -> nn.Module:
     gnn = {"gat": _GATConv, "transformer": _TransformerConv}
     assert gnn_layer in gnn.keys(), f"Wrong GNN layer class: {gnn_layer}"
     return gnn[gnn_layer]
@@ -99,10 +99,13 @@ class GNNEncoder(nn.Module):
         ff_list = []
         for _ in range(self.num_layers):
             gnn_layer = _TransformerConv(
-                in_channels=self.embed_dim, out_channels=self.embed_dim // self.heads, heads=self.heads,
+                in_channels=self.embed_dim,
+                out_channels=self.embed_dim // self.heads,
+                heads=self.heads,
+                edge_dim=self.embed_dim,
             )
             gnn_layer_list.append(gnn_layer)
-            norm = self.norm_class(self.embed_dim)
+            norm = self.norm_class(in_channels=self.embed_dim)
             norm_list.append(norm)
             feed_forward = nn.Sequential(
                 nn.Linear(self.embed_dim, feed_forward_hidden),
@@ -116,18 +119,7 @@ class GNNEncoder(nn.Module):
         self.norm_list = nn.ModuleList(norm_list)
         self.ff_list = nn.ModuleList(ff_list)
 
-    def forward(self, x: torch.Tensor, edge_index: torch.Tensor) -> torch.Tensor:
-
-        for gnn_layer, norm, ff in zip(self.gnn_layer_list, self.norm_list, self.ff_list):
-            # identity = x
-            x = gnn_layer(x, edge_index)
-            x = norm(x)
-            x = ff(x)
-            # x += identity
-
-        return x
-
-    def encode(self, data: Batch) -> Tuple[torch.Tensor, torch.Tensor]:
+    def forward(self, data: Batch) -> Tuple[torch.Tensor, torch.Tensor]:
         """Encode graph input features.
         
         Get node_embedding, graph_embedding.
@@ -137,17 +129,22 @@ class GNNEncoder(nn.Module):
             data: a torch_geometric batch of graphs
 
         Returns:
-            dense_x: encoded graph node features, shape->[batch_size, node_num, embed_dim]
+            node_embeddings: encodded graph node features, shape->[batch_size, node_num, embed_dim]
             graph_feat: graph features produced by pooling function, shape->[batch_size, embed_dim]
         """
         x = data.x
         edge_index = data.edge_index
+        edge_attr = data.edge_attr
 
-        x = self(x, edge_index)
-        dense_x, dense_mask = to_dense_batch(x, data.batch)
+        for gnn_layer, norm, ff in zip(self.gnn_layer_list, self.norm_list, self.ff_list):
+            x = gnn_layer(x, edge_index, edge_attr)
+            x = norm(x)
+            x = ff(x)
+
+        node_embeddings, dense_mask = to_dense_batch(x, data.batch)
         assert dense_mask.all(), "For now only support a batch of graphs with the same number of nodes"
 
         # Pooling graph features
         graph_feat = self.pooling_func(x, data.batch)
 
-        return dense_x, graph_feat
+        return (node_embeddings, graph_feat)

models/tsp_agent.py
@@ -3,19 +3,32 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from typing import Any, Tuple
+from typing import Any, NamedTuple, Tuple
 from torch_geometric.data import Data, Batch
 from .encoder import cal_size_list, MLP, GNNEncoder
 from .decoder import AttentionDecoder
+
+
+class AttentionInfoFixed(NamedTuple):
+    """
+    Context for AttentionModel decoder that is fixed during decoding so can be precomputed/cached
+    This class allows for efficient indexing of multiple Tensors at once
+    """
+
+    node_embeddings: torch.Tensor
+    graph_context_projected: torch.Tensor
+    glimpse_key: torch.Tensor
+    glimpse_val: torch.Tensor
+    logit_key: torch.Tensor
 
 
 class TSPAgent(nn.Module):
     def __init__(self, args: Any) -> None:
         super().__init__()
         self.args = args
-        self.input_dim = args.input_dim
+        self.node_dim = args.node_dim
+        self.edge_dim = args.edge_dim
         self.embed_dim = args.embed_dim
-        self.num_embed_layers = args.num_embed_layers
         self.num_gnn_layers = args.num_gnn_layers
         self.encoder_num_heads = args.encoder_num_heads
         self.decoder_num_heads = args.decoder_num_heads
@@ -24,9 +37,10 @@ class TSPAgent(nn.Module):
         self.pooling_method = args.pooling_method
         self.normalization = args.normalization
         self.set_decode_type(args.decode_type)
-        linear_size_list = cal_size_list(self.input_dim, self.embed_dim, self.num_embed_layers)
 
-        self.linear_embedder = MLP(linear_size_list, bias=self.bias, last_activation=nn.Identity)
+        self.node_embedder = nn.Linear(self.node_dim, self.embed_dim)
+        self.edge_embedder = nn.Linear(self.edge_dim, self.embed_dim)
+
         self.encoder = GNNEncoder(
             self.embed_dim,
             self.num_gnn_layers,
@@ -51,17 +65,35 @@ class TSPAgent(nn.Module):
         self.decode_type = decode_type
 
     def init_embed(self, data: Batch) -> Batch:
-        assert data.x.size(-1) == self.input_dim
-        x = self.linear_embedder(data.x)
+        assert data.pos.size(-1) == self.node_dim
+        assert data.edge_attr.size(-1) == self.edge_dim
+
+        x = self.node_embedder(data.pos)
+        edge_attr = self.edge_embedder(data.edge_attr)
+
         d = data.clone()
         d.x = x
+        d.edge_attr = edge_attr
 
         return d
 
-    def forward(
-        self, state: TSPState, dense_x: torch.Tensor, graph_feat: torch.Tensor
-    ) -> Tuple[torch.Tensor, torch.Tensor]:
-        batch_size, num_nodes, embed_dim = dense_x.shape
+    def precompute_fixed(self, node_embeddings: torch.Tensor, graph_feat: torch.Tensor) -> AttentionInfoFixed:
+        graph_context = self.graph_proj(graph_feat)
+        glimpse_K, glimpse_V, logit_K = self.project_node_embeddings(node_embeddings).chunk(3, dim=-1)
+        glimpse_K = glimpse_K.permute(1, 0, 2).contiguous()  # (num_nodes, batch_size, embed_dim)
+        glimpse_V = glimpse_V.permute(1, 0, 2).contiguous()  # (num_nodes, batch_size, embed_dim)
+        logit_K = logit_K.contiguous()  # ((batch_size, num_nodes, embed_dim))
+
+        return AttentionInfoFixed(
+            node_embeddings=node_embeddings,
+            graph_context_projected=graph_context,
+            glimpse_key=glimpse_K,
+            glimpse_val=glimpse_V,
+            logit_key=logit_K,
+        )
+
+    def forward(self, state: TSPState, fixed: AttentionInfoFixed) -> Tuple[torch.Tensor, torch.Tensor]:
+        batch_size, num_nodes, _ = fixed.node_embeddings.shape
         assert state.first_node is None or list(state.first_node.shape) == list(state.pre_node.shape) == [
             batch_size,
             1,
@@ -73,28 +105,25 @@ class TSPAgent(nn.Module):
         ], f"{state.avail_mask.shape}-{[batch_size, num_nodes]}"
 
         # Transform node features for attention compute
-        query = self._make_query(state, dense_x, graph_feat)
-        mask = state.avail_mask
-        glimpse_K, glimpse_V, logit_K = self.project_node_embeddings(dense_x).chunk(3, dim=-1)
-        glimpse_K = glimpse_K.permute(1, 0, 2).contiguous() # (num_nodes, batch_size, embed_dim)
-        glimpse_V = glimpse_V.permute(1, 0, 2).contiguous()
+        query = self._make_query(state, fixed.node_embeddings, fixed.graph_context_projected)
 
-        log_p = self.decoder(query, glimpse_K, glimpse_V, logit_K, attn_mask=~mask.unsqueeze(1))
-        selected = self._select_node(log_p, mask)
+        mask = ~state.avail_mask.unsqueeze(1)  # (batch_size, 1, num_nodes)
+
+        log_p = self.decoder(query, fixed.glimpse_key, fixed.glimpse_val, fixed.logit_key, mask)
+        selected = self._select_node(log_p, mask.squeeze())
 
         return selected, log_p
 
-    def _make_query(self, state: TSPState, dense_x: torch.Tensor, graph_feat: torch.Tensor) -> torch.Tensor:
+    def _make_query(self, state: TSPState, node_embeddings: torch.Tensor, graph_context: torch.Tensor) -> torch.Tensor:
         r"""
         query: :math:`(L, N, E)` where L is the target sequence length, N is the batch size, E is
             the embedding dimension.
         """
-        batch_size, num_nodes, embed_dim = dense_x.shape
+        batch_size, _, embed_dim = node_embeddings.shape
 
-        graph_context = self.graph_proj(graph_feat)
         if state.first_node is not None:
             step_context = self.step_proj(
-                dense_x.gather(
+                node_embeddings.gather(
                     1, torch.cat((state.first_node, state.pre_node), 1)[:, :, None].expand(batch_size, 2, embed_dim),
                 ).view(batch_size, -1)
             )
@@ -109,15 +138,15 @@ class TSPAgent(nn.Module):
     def _select_node(self, log_p: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
         assert log_p.shape == mask.shape, f"{log_p.shape}, {mask.shape}"
         probs = log_p.exp()
-        assert (probs == probs).all(), "Probs should not contain any nans"
+        assert not torch.isnan(probs).any(), "Probs should not contain any nans"
 
         if self.decode_type == "greedy":
             _, selected = probs.max(1)
             selected = selected.unsqueeze(-1)
-            assert mask.gather(1, selected).all(), "Decode greedy: infeasible action has maximum probability"
+            assert not mask.gather(1, selected).any(), "Decode greedy: infeasible action has maximum probability"
         elif self.decode_type == "sampling":
             selected = probs.multinomial(1)
-            while not mask.gather(1, selected).all():
+            while mask.gather(1, selected).any():
                 print("Sampled bad values, resampling!")
                 print(selected)
                 print(mask)

models/tsp_baseline.py
@@ -11,7 +11,7 @@ from .encoder import cal_size_list, MLP, GNNEncoder
 
 
 class Baseline(object):
-    def eval(self, x, c):
+    def evaluate(self, x, c):
         raise NotImplementedError("Override this method")
 
     def epoch_callback(self, model, epoch):
@@ -50,7 +50,7 @@ class CriticBaseline(nn.Module, Baseline):
 
         return out
 
-    def eval(self, data: Batch, target: torch.Tensor) -> torch.Tensor:
+    def evaluate(self, data: Batch, target: torch.Tensor) -> torch.Tensor:
 
         out = self.forward(data)
 
@@ -66,7 +66,7 @@ class ExponentialBaseline(Baseline):
         self.beta = args.exp_beta
         self.v = None
 
-    def eval(self, data: Batch, target: torch.Tensor) -> torch.Tensor:
+    def evaluate(self, data: Batch, target: torch.Tensor) -> torch.Tensor:
 
         if self.v is None:
             v = target.mean()
@@ -74,7 +74,7 @@ class ExponentialBaseline(Baseline):
             v = self.beta * self.v + (1.0 - self.beta) * target.mean()
 
         self.v = v.detach()  # Detach since we never want to backprop
-        return self.v, torch.tensor(0, dtype=torch.float, requires_grad=True)  # No loss
+        return self.v, 0  # No loss
 
     def state_dict(self):
         return {"v": self.v}

rl_algorithms/reinforce.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from environments import TSPEnv
+from pprint import pprint
 from torch_geometric.data import Batch, Data
 from torch_geometric.utils import to_dense_batch
 from torch.utils.tensorboard.writer import SummaryWriter
@@ -48,9 +49,10 @@ def reinforce_train_batch(
     done = False
     state = env.reset(node_pos)
     embed_data = model.init_embed(batch)
-    dense_x, graph_feat = model.encoder.encode(embed_data)
+    node_embeddings, graph_feat = model.encoder(embed_data)
+    fixed = model.precompute_fixed(node_embeddings, graph_feat)
     while not done:
-        action, log_p = model(state, dense_x, graph_feat)
+        action, log_p = model(state, fixed)
         state, reward, done, _ = env.step(action)
         log_p_s.append(log_p)
         action_s.append(action)
@@ -60,9 +62,9 @@ def reinforce_train_batch(
     # Calculate policy's log_likelihood and reward
     log_likelihood = _calc_log_likelihood(log_p, a)
     # reward is a negative value of tour lenth
-    cost = -(reward_s[-1].unsqueeze(-1))
     # let baseline to predict positive value
-    bl_val, bl_loss = baseline.eval(batch, cost)
+    cost = -(reward_s[-1])
+    bl_val, bl_loss = baseline.evaluate(batch, cost)
     rl_loss = ((cost - bl_val) * log_likelihood).mean()
     loss = rl_loss + bl_loss
 
@@ -85,6 +87,7 @@ def reinforce_train_batch(
             bl_loss=bl_loss,
             log_p=log_p,
             logger=logger,
+            args=args,
         )
 
 
@@ -98,7 +101,20 @@ def _calc_log_likelihood(_log_p, a):
     return log_p.sum(1)
 
 
-def log_values(cost, grad_norms, bl_val, epoch, batch_id, step, log_likelihood, reinforce_loss, bl_loss, log_p, logger: SummaryWriter):
+def log_values(
+    cost,
+    grad_norms,
+    bl_val,
+    epoch,
+    batch_id,
+    step,
+    log_likelihood,
+    reinforce_loss,
+    bl_loss,
+    log_p,
+    logger: SummaryWriter,
+    args,
+):
     avg_cost = cost.mean().item()
     bl_cost = bl_val.mean().item()
     grad_norms, grad_norms_clipped = grad_norms
@@ -118,10 +134,10 @@ def log_values(cost, grad_norms, bl_val, epoch, batch_id, step, log_likelihood, 
 
     logger.add_scalar("actor_loss", reinforce_loss.item(), step)
     logger.add_scalar("nll", -log_likelihood.mean().item(), step)
-
-    logger.add_scalar("critic_loss", bl_loss.item(), step)
+    # if args.baseline == "critic":
+    #     logger.add_scalar("critic_loss", bl_loss.item(), step)
     if not batch_id % 100:
-        num_step, num_graph, num_node = log_p.shape
+        num_graph, num_step, num_node = log_p.shape
         logger.add_histogram("first_step_prob", log_p.cpu()[0][0].exp().squeeze(), step)
-        logger.add_histogram("mid_step_prob", log_p.cpu()[num_step//2][0].exp().squeeze(), step)
-        logger.add_histogram("last_step_prob", log_p.cpu()[-1][0].exp().squeeze(), step)
+        logger.add_histogram("mid_step_prob", log_p.cpu()[0][num_step // 2].exp().squeeze(), step)
+        logger.add_histogram("last_step_prob", log_p.cpu()[0][-1].exp().squeeze(), step)